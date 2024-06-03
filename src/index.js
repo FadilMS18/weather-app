abb6af9ea959031422471cdbde63bb372d15ed1b
FadilMS18 <muhfadilms2418@gmail.com>
2024-06-03 14:49:49 +0800

    added mock layout for the page
    added navbar and main section for searching
    now user can search for correct location
    but will return error for incorrect location

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './css/styles.css'
+import './css/loading.css'
+import './css/nav.css'
 import {x} from './modules/first'
 import { format } from 'date-fns'
 console.log(x)
@@ -6,20 +8,8 @@ console.log('Hello, world!')
 
 
 
-async function weatherTest(){
-    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=4c9280a21b7640efb4e51904242205&q=${search.value}&days=3&aqi=no&alerts=no`)
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    let forecast = await data.forecast
-    console.log(forecast)
-    let bgResponse = await fetch('https://api.giphy.com/v1/gifs/dU97uV3UyP0ly?api_key=3cgs9hdwFdzL6pSZSImZnFRDPzsKdybB')
-    let bg = await bgResponse.json()
-    document.body.style.backgroundImage = `url(${bg.data.images.original.url})`
-} 
 
-const cityInfo = document.querySelector('#city')
-cityInfo.innerText = `Makassar, South Sulawesi Indonesia \n Thursday, 30 May 2024`
+
 // weatherTest()
 
 // Remember: Weather API only allow us to get 3 days ahead forecast
@@ -38,3 +28,30 @@ console.log(Boolean(obj.notNato))
 
 // TODO: search for a way that your gif will match your theme 
 // TODO: make the weather api working
+// TODO: Use figma to design phone and tablet layout
+// TODO: Make a theme for day and night 
+
+const root = document.querySelector(":root")
+root.classList.add('dark')
+
+
+window.addEventListener('scroll', navbarHandler)
+
+function navbarHandler(){
+    const navbar = document.querySelector('#nav')
+    if(window.scrollY > 400){
+        console.log(window)
+        navbar.classList.contains('popNavbar') 
+        ? navbar.classList.remove('popNavbar')
+        : null ;
+    }
+    if(window.scrollY <= 400 ){
+        !navbar.classList.contains('popNavbar') 
+        ? navbar.classList.add('popNavbar')
+        : null ;
+    }  
+}
+
+// scrollDown.addEventListener('click', ()=>{
+//     navbar.classList.toggle('popNavbar')
+// })
--- src/modules/first.js
+++ src/modules/first.js
@@ -1,3 +1,100 @@
+import { format } from "date-fns"
+
 let x = 'this is from first js'
+
+console.log('hello, world!')
+
+let desiredTempCelcius = true
+
+class Weather{
+    static getLocation({location}){
+        return location
+    }
+    static getCurrent({current}){
+        return current
+    }
+    static getForecast({forecast}){
+        return forecast
+    }
+}
+
+const searchBar = document.querySelector('#search')
+const searchButton = document.querySelector('#search-button')
+
+async function getWeatherData(){
+    const loadingScreen = document.querySelector('#loading-screen')
+    loadingScreen.classList.remove('finish')
+    
+    try{
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=4c9280a21b7640efb4e51904242205&q=${searchBar.value}&days=3&aqi=no&alerts=no`, {mode:'cors'})
+        if(!response.ok){
+            throw new Error(response.statusText);
+        }
+        const data = await response.json()
+        searchBar.value = ''
+        loadingScreen.classList.add('finish')
+        return data    
+    }catch(error){
+        !loadingScreen.classList.contains('finish') ? loadingScreen.classList.add('finish') : null
+        searchBar.value = ''
+        alert(error)
+        
+    }
+    
+    
+
+    
+}
+
+async function leftContainerHandler(){
+    if(checkInputValue(searchBar)){ return }
+    const data = await getWeatherData()
+    cityNameHandler(data)
+    conditionHandler(data)
+}
+
+
+function cityNameHandler(data){
+    const {location} = data
+    const cityInfo = document.querySelector('#city')
+    const localTime = format(location.localtime, 'EEEE, do LLL yyyy ')
+    cityInfo.innerText = `${location.name}, ${location.region} ${location.country} \n ${localTime}`
+
+    
+    
+
+
+}
+
+function conditionHandler(data){
+    const {current} = data
+    const condition = document.querySelector('#condition')
+    condition.textContent = current.condition.text
+    const temp = document.querySelector('#temp-info > h2')
+    temp.textContent = `${current.temp_c} °C`
+}
+
+function checkInputValue(input){
+    if(input.value === ''){
+        console.log('Please insert a city name')
+        return true
+    }
+    return false
+}
+
+searchButton.addEventListener('click', ()=>{
+    leftContainerHandler()
+})
+
+searchBar.addEventListener('keyup',  (e)=>{
+    if(e.key !== 'Enter'){
+        return
+    }
+    leftContainerHandler()
+})
+
+// TODO: Make one async function for content container and make mega function that will pop up the loading and await all async function
+// TODO: figure out how to stop the problem on error at getweatherdata bad response
+
 
 export { x }
