f6bf500fabdcb8c1d7e8720173c8b743aa935cc7
FadilMS18 <muhfadilms2418@gmail.com>
2024-08-30 11:29:21 +0800

    Fixed navbar didn't show up
    Fixed navbar issue where its randomly popped up
    reworking navbar code for readability

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { WeatherHandler, desiredTempCelsius } from "./modules/weather";
 // TODO: Use figma to design phone and tablet layout
 
 window.addEventListener("scroll", navbarHandler);
-window.addEventListener("DOMContentLoaded", loadFirstPage, {once: true});
+window.addEventListener("DOMContentLoaded", loadFirstPage, { once: true });
 
 // default city searcher that is Amsterdam
 function loadFirstPage() {
@@ -16,12 +16,10 @@ function loadFirstPage() {
 }
 
 // Scroll down function, pop the navbar when ever the user past the first search box
+
 function navbarHandler() {
   const navbar = document.querySelector("#nav");
-  if (window.scrollY > 400) {
-    navbar.classList.remove("popNavbar");
-  }
-  if (window.scrollY <= 400) {
-    navbar.classList.add("popNavbar");
-  }
+  window.scrollY > 300
+    ? navbar.classList.remove("popNavbar")
+    : navbar.classList.add("popNavbar");
 }
